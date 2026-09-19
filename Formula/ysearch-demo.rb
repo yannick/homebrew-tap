ad6bd2213e52a6c8a05e4d209174fecee658aa71
Yannick Koechlin <yannick@koechlin.info>
2026-09-19 18:16:48 +0200

    ysearch-demo 0.2.0
    A space between terms now means AND, not OR.

--- Formula/ysearch-demo.rb
+++ Formula/ysearch-demo.rb
@@ -6,9 +6,9 @@
 class YsearchDemo < Formula
   desc "Query the public YoloSearch FineWeb benchmark corpus from your terminal"
   homepage "https://yolosearch.teixos.net"
-  url "https://git.teixos.net/api/packages/yannick/generic/ysearch-demo-release/v0.1.0-6-g8d35971/ysearch-demo-v0.1.0-6-g8d35971-macos-universal.tar.gz"
-  version "0.1.0-6-g8d35971"
-  sha256 "9e93e2d307e3ec290f00abe29febd679113b63989e3f0779be84a4ac89f7c7ea"
+  url "https://git.teixos.net/api/packages/yannick/generic/ysearch-demo-release/v0.2.0/ysearch-demo-v0.2.0-macos-universal.tar.gz"
+  version "0.2.0"
+  sha256 "6396002d5ae5b37a54d6065fd7b45e04689f008b86283df1fda79cd6efa006a2"
 
   depends_on :macos
 
